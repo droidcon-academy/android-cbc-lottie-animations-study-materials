30b05e7702f7617dc9ec4ad8475a0815385bb16d
Ian Okumu <okumu.otsembo@gmail.com>
2023-10-18 14:18:08 +0300

    clean up starter code.

--- source-code/starter-project/app/src/main/java/com/droidcon/lottieanimationcompose/MainScreen.kt
+++ source-code/starter-project/app/src/main/java/com/droidcon/lottieanimationcompose/MainScreen.kt
@@ -4,7 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,6 +23,6 @@ fun MainScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // We will put how code here
+
     }
 }
